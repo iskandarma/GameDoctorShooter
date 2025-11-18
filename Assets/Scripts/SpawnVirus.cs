065be2f920143ac8b714874c03f0259143d6e249
apriandirian93 <apriandi.rian93@gmail.com>
2025-11-18 11:52:43 +0700

    Update SpawnVirus.cs

--- Assets/Scripts/SpawnVirus.cs
+++ Assets/Scripts/SpawnVirus.cs
@@ -73,7 +73,7 @@ public class SpawnVIrus : MonoBehaviour
                 virus.GetComponent<SpriteRenderer>().sprite = dbBesar.ListVirusBesar[index];
             }
 
-            yield return new WaitForSeconds(1.5f);
+            yield return new WaitForSeconds(1);
             }
     }
 }
